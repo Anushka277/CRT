8d80f08bfd7e268ed258655927a9f10f715e797c
Anushka277 <anushkayadav2772006@gmail.com>
2026-03-02 11:03:12 +0530

    message

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,10 +1,10 @@
 def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    average = (n1 + n2 + n3) / 3
-    status = "Pass" if average >= 40 else "Fail"
+    avg = (n1 + n2 + n3) / 3
+    avg = int(avg * 100) / 100
     
-    return f"Average grade: {average:.2f}, Status: {status}"
-
-if __name__ == '__main__':
-    name = input()
-    n1, n2, n3 = list(map(int, input().split()))
-    print(Student_Grade_System(name, n1, n2, n3))
+    if avg >= 40:
+        status = "Pass"
+    else:
+        status = "fail"
+    
+    return f"Average grade: {avg}, Status: {status}"
